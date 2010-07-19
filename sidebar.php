bb4d1496b70f7624e97fb0059dc3360cc654a138
Rebecca <h4xnoodle@gmail.com>
2010-07-19 01:12:06 +0000

    Sidebar update: Add teamster and tie guard twitter feeds on team pages.

--- sidebar.php
+++ sidebar.php
@@ -7,7 +7,6 @@
 if(basename($_SERVER['PHP_SELF'],'.php') == "faq") { ?>
 
 
-
 <?php } else if(basename($_SERVER['PHP_SELF'],'.php') == "main" && !($_SERVER['QUERY_STRING'])) { ?>
 	<h2 class="twitter">Twitter</h2>
 	<script type="text/javascript">
@@ -40,7 +39,71 @@ if(basename($_SERVER['PHP_SELF'],'.php') == "faq") { ?>
 		  }
 		}).render().setUser('mathorientation').start();
 	</script>
-<?php } ?>
-<?php if(!(basename($_SERVER['PHP_SELF'],'.php') == "main" && !($_SERVER['QUERY_STRING']))) { ?>
-<?php } ?>
+<?php } elseif(basename($_SERVER['PHP_SELF'],'.php') == "teams" && ($_SERVER['QUERY_STRING'] == 'team_rocket') { ?>
+	<h2 class="twitter">Twitter Feed</h2>
+	<script type="javascript">
+	new TWTR.Widget({
+	  version: 2,
+	  type: 'profile',
+	  rpp: 4,
+	  interval: 6000,
+	  width: 250,
+	  height: 300,
+	  theme: {
+		shell: {
+		  background: '#333333',
+		  color: '#ffffff'
+		},
+		tweets: {
+		  background: '#000000',
+		  color: '#ffffff',
+		  links: '#4aed05'
+		}
+	  },
+	  features: {
+		scrollbar: false,
+		loop: false,
+		live: false,
+		hashtags: true,
+		timestamp: true,
+		avatars: false,
+		behavior: 'all'
+	  }
+	}).render().setUser('uwteamsters').start();
+	</script>
+<?php } elseif(basename($_SERVER['PHP_SELF'],'.php') == "teams" && ($_SERVER['QUERY_STRING'] == 'elite_four') { ?>
+	<h2 class="twitter">Twitter Feed</h2>
+	<script type="javascript">
+	new TWTR.Widget({
+	  version: 2,
+	  type: 'profile',
+	  rpp: 4,
+	  interval: 6000,
+	  width: 250,
+	  height: 300,
+	  theme: {
+		shell: {
+		  background: '#333333',
+		  color: '#ffffff'
+		},
+		tweets: {
+		  background: '#000000',
+		  color: '#ffffff',
+		  links: '#4aed05'
+		}
+	  },
+	  features: {
+		scrollbar: false,
+		loop: false,
+		live: false,
+		hashtags: true,
+		timestamp: true,
+		avatars: false,
+		behavior: 'all'
+	  }
+	}).render().setUser('tie_guard').start();
+	</script>
+<?php } 
+if(!(basename($_SERVER['PHP_SELF'],'.php') == "main" && !($_SERVER['QUERY_STRING']))) { ?>
+} ?>
 </div>
